35a5df92b03065021dc7b597b52538c7b73e670a
amandinek <karambiziga@gmail.com>
2019-09-01 23:40:02 +0200

    add a directive style

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  // highlight: any;
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+  @HostListener("mousenter") onMouseEnter(){
+    this.highlight("highlight");
+  }
+  @HostListener("mouseleave") onmouseleave(){
+    this.highlight(null);
+  }
+  private highlight(highlight:string){
+    this.elem.nativeElement.style.backgroundcolor="blue";
+  }
+
 
 }
+
+
