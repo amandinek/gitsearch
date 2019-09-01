b7f6f08535409510a0b8fec51de5396a0074c5e3
amandinek <karambiziga@gmail.com>
2019-09-02 01:10:11 +0200

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   clientId:"5002525f535295821679",
-  clientKey:"c36ee9c48419d167bc0fa0adee0f0beb25d03f4b",
+  ClientKey:"c36ee9c48419d167bc0fa0adee0f0beb25d03f4b",
 };
